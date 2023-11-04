5f7e7849453091518c417d775e1fb300b670d18b
Francesco <francesco.spezzano@outlook.com>
2023-11-04 16:25:07 +0100

    modiche al main

--- src/main/java/it/uniroma3/hw3/Main.java
+++ src/main/java/it/uniroma3/hw3/Main.java
@@ -3,6 +3,6 @@ package it.uniroma3.hw3;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Ciao a tutti");
     }
 }
