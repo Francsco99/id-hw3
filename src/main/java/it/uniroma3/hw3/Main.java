1fd4b7d611a8407565d7951b10b22b09a53e8196
Francesco <francesco.spezzano@outlook.com>
2023-11-06 15:19:34 +0100

    refactoring nome classe

--- src/main/java/it/uniroma3/hw3/Main.java
+++ src/main/java/it/uniroma3/hw3/Main.java
@@ -4,7 +4,7 @@ public class Main {
     final static String TABLE_PATH = System.getProperty("user.dir")+"/tabella.json";
     final static String INDEX_PATH = System.getProperty("user.dir")+"/index";
     public static void main(String[] args) {
-        TableReader tr = new TableReader();
+        TableIndexer tr = new TableIndexer();
         tr.tableIndexer(TABLE_PATH,INDEX_PATH);
     }
 }
