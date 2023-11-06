dcaa970295ea31771463f20b9aef833b146a3162
Francesco <francesco.spezzano@outlook.com>
2023-11-06 15:18:30 +0100

    commenti

--- src/main/java/it/uniroma3/hw3/TableReader.java
+++ src/main/java/it/uniroma3/hw3/TableReader.java
@@ -28,14 +28,15 @@ public class TableReader {
             // Apre un file JSON contenente tabelle
             BufferedReader reader = new BufferedReader(new FileReader(jsonPath));
             String line;
-            int j = 0; // Contatore per le tabelle
+            //int j = 0; // Contatore per le tabelle
 
-
+            // Directory Lucene per indice
             Directory indexDirectory = FSDirectory.open(Paths.get(indexPath));
+            // Writer per scrivere sull'indice
             IndexWriterConfig indexConfig = new IndexWriterConfig(new StandardAnalyzer());
             IndexWriter writer = new IndexWriter(indexDirectory,indexConfig);
+            // Pulisco il vecchio indice
             writer.deleteAll();
-
 
             while ((line = reader.readLine()) != null) {
                 // Analizza la riga corrente come oggetto JSON
@@ -46,12 +47,13 @@ public class TableReader {
                 int maxCols = maxDimensions.get("column").getAsInt(); // Ottiene il numero massimo di colonne
                 JsonArray cells = jsonTable.getAsJsonArray("cells"); // Ottiene l'array di celle
 
-                System.out.println("Tabella " + j + " " + tableId + " " + maxRows + " " + maxCols);
-                j++;
+                //Stampa l'id della tabella corrente
+                //System.out.println("Tabella " + j + " " + tableId + " " + maxRows + " " + maxCols);
+                //j++;
+
                 // Crea una mappa per memorizzare i valori "cleanedText" per ciascuna colonna
                 Map<Integer, List<String>> columnValues = new HashMap<>();
 
-                Document doc = new Document();
                 // Riempie la mappa con i valori "cleanedText" per ciascuna colonna
                 for (int i = 0; i < cells.size(); i++) {
                     JsonObject cell = cells.get(i).getAsJsonObject();
@@ -64,49 +66,31 @@ public class TableReader {
                     }
                     columnValues.get(col).add(cleanedText);
                 }
-
+                // Nuovo documento Lucene per l'indice
+                Document doc = new Document();
+                // Aggiunge il campo id
                 doc.add(new TextField("id",tableId,Field.Store.YES));
-                // Itera sulle colonne per stampare il contenuto
+                // Itera sulle colonne della mappa per salvare il contenuto sull'indice
                 for (int c = 0; c <= maxCols; c++) {
                     if (columnValues.containsKey(c)) {
                         List<String> columnData = columnValues.get(c);
                         String columnDataString = String.join(", ",columnData);
+                        // Aggiunge il campo colonna con il relativo contenuto della colonna
                         doc.add(new TextField("column"+c,columnDataString,Field.Store.YES));
-                        System.out.println("Column " + c + ": " + String.join(", ", columnData));
+                        //System.out.println("Column " + c + ": " + String.join(", ", columnData));
                     }
                 }
+                // Scrivi sull'indice
                 writer.addDocument(doc);
                 writer.commit();
-
             }
+            // Chiudi l'indice
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
-    /*    public void readTable(String filePath) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            String line;
-            int j = 0;
-            while ((line = reader.readLine()) != null) {
-                JsonObject jsonTable = JsonParser.parseString(line).getAsJsonObject();
-                String tableId = jsonTable.get("id").getAsString();
-
-                JsonArray cells = jsonTable.getAsJsonArray("cells");
-                for (int i = 0; i < cells.size(); i++) {
-                    JsonObject cell = cells.get(i).getAsJsonObject();
-                    String columnName = cell.get("cleanedText").getAsString();
-
-                }
-                System.out.println("tabella " + j + " " + tableId);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
 
 
 
