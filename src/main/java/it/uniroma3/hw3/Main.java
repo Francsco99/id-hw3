d8bdbf89799f24b7eab0f6b3725697812762e28a
franc <francesco.salienti@gmail.com>
2023-11-08 20:20:30 +0100

    commit

--- src/main/java/it/uniroma3/hw3/Main.java
+++ src/main/java/it/uniroma3/hw3/Main.java
@@ -1,28 +1,52 @@
 package it.uniroma3.hw3;
 
-import java.util.List;
+import org.apache.lucene.codecs.Codec;
+import org.apache.lucene.codecs.simpletext.SimpleTextCodec;
+
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
-    final static String TABLE_PATH = System.getProperty("user.dir")+"/tavola.json";
-    final static String INDEX_PATH = System.getProperty("user.dir")+"/index";
-    public static void main(String[] args) {
-        //TableIndexer tr = new TableIndexer();
-        //tr.tableIndexer(TABLE_PATH,INDEX_PATH);
-        InvertedIndex inv = new InvertedIndex();
-        Map<String, List<String>> invertedIndex = inv.inverted(TABLE_PATH,INDEX_PATH);
+    final static String TABLE_PATH = "C:/Users/franc/Desktop/hw3_dati/tables/tables.json";
+    final static String INDEX_PATH = "C:/Users/franc/Desktop/hw3_dati/tables/indice";
 
-        int count = 0;
-        for (Map.Entry<String, List<String>> entry : invertedIndex.entrySet()) {
-            if (count >= 10) {
+    public static void main(String[] args) throws IOException {
+        TableIndexer tr = new TableIndexer();
+        Searcher search = new Searcher();
+        Codec codec = new SimpleTextCodec();
+        Set<String> set = new HashSet<>();
+
+        //tr.tableIndexer(TABLE_PATH, INDEX_PATH, codec);
+        String s = "crimes";
+        String c = "plural";
+        String d = "singular";
+
+        set.add(s);
+        set.add(c);
+
+        set.add(d);
+
+        search.read_from_index(INDEX_PATH, set);
+
+
+        /*int count = 0;
+        for (Map.Entry<String, Set<String>> entry : invertedIndex.entrySet()) {
+            if (count >= 50) {
                 break;
             }
             String key = entry.getKey();
-            List<String> columns = entry.getValue();
+            Set<String> columns = entry.getValue();
             System.out.println("Element: " + key);
             System.out.println("Columns: " + columns);
+            System.out.println("\n\n");
+
             count++;
         }
+        int numeroDiChiavi = invertedIndex.size();
+
+        System.out.println("Numero di chiavi nell'HashMap: " + numeroDiChiavi);*/
     }
 }
