e7c4c4c28f126feeb562762ba6d44d21cc5a6f98
Francesco <francesco.spezzano@outlook.com>
2023-11-08 22:35:46 +0100

    commenti main

--- src/main/java/it/uniroma3/hw3/Main.java
+++ src/main/java/it/uniroma3/hw3/Main.java
@@ -3,9 +3,7 @@ package it.uniroma3.hw3;
 import org.apache.lucene.codecs.Codec;
 import org.apache.lucene.codecs.simpletext.SimpleTextCodec;
 
-import java.io.IOException;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class Main {
@@ -13,14 +11,15 @@ public class Main {
     final static String INDEX_PATH = System.getProperty("user.dir")+"/index";
     final static String TABLE_PATH = System.getProperty("user.dir")+"/tables.json";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         TableIndexer tr = new TableIndexer();
-        Searcher search = new Searcher();
+        //Searcher search = new Searcher();
         Codec codec = new SimpleTextCodec();
-        Set<String> set = new HashSet<>();
+        //Set<String> set = new HashSet<>();
 
         tr.tableIndexer(TABLE_PATH, INDEX_PATH, codec);
-/*        String s = "crimes";
+
+/*      String s = "crimes";
         String c = "plural";
         String d = "singular";
 
@@ -31,22 +30,5 @@ public class Main {
 
         search.read_from_index(INDEX_PATH, set);*/
 
-
-        /*int count = 0;
-        for (Map.Entry<String, Set<String>> entry : invertedIndex.entrySet()) {
-            if (count >= 50) {
-                break;
-            }
-            String key = entry.getKey();
-            Set<String> columns = entry.getValue();
-            System.out.println("Element: " + key);
-            System.out.println("Columns: " + columns);
-            System.out.println("\n\n");
-
-            count++;
-        }
-        int numeroDiChiavi = invertedIndex.size();
-
-        System.out.println("Numero di chiavi nell'HashMap: " + numeroDiChiavi);*/
     }
 }
