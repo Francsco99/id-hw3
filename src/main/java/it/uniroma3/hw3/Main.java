4c1596a94358874b8a89070f5229fd2bf0e5ba99
Francesco <francesco.spezzano@outlook.com>
2023-11-09 11:38:40 +0100

    indicizzazione solo se cartella indice vuota

--- src/main/java/it/uniroma3/hw3/Main.java
+++ src/main/java/it/uniroma3/hw3/Main.java
@@ -1,5 +1,6 @@
 package it.uniroma3.hw3;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,13 +11,20 @@ public class Main {
     final static String TABLE_PATH = System.getProperty("user.dir")+"/tables.json";
 
     public static void main(String[] args) throws IOException {
+        File indexFolder = new File(INDEX_PATH);
+        File[] files = indexFolder.listFiles();
         TableIndexer tr = new TableIndexer();
         Searcher search = new Searcher();
         Set<String> set = new HashSet<>();
 
-        //tr.tableIndexer(TABLE_PATH, INDEX_PATH);
+if(files != null && files.length==0) {
+    tr.tableIndexer(TABLE_PATH, INDEX_PATH);
+}
+else{
+    System.out.println("La cartella INDEX_PATH non è vuota. Non verrà eseguita l'indicizzazione.");
+}
 
-     String s = "crimes";
+        String s = "War crimes";
         String c = "plural";
         String d = "singular";
 
