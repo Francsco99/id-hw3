cc23199ce002503fa9590633f9b7801ee04ed16d
Francesco <francesco.spezzano@outlook.com>
2023-11-09 16:24:31 +0100

    modifiche, merge con ramo fra

--- src/main/java/it/uniroma3/hw3/PrintColored.java
+++ src/main/java/it/uniroma3/hw3/PrintColored.java
@@ -5,9 +5,13 @@ public class PrintColored {
     public  void printColored(String message, String color) {
         String ANSI_RESET = "\u001B[0m";
         String ANSI_GREEN = "\u001B[32m";
+        String ANSI_RED =  "\u001B[31m";
 
         String colorCode;
         switch (color.toLowerCase()) {
+            case "red":
+                colorCode = ANSI_RED;
+                break;
             case "green":
                 colorCode = ANSI_GREEN;
                 break;
